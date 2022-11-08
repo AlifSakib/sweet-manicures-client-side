b1f41a43baec39bdc18f43ea795d46e9325463e6
AlifSakib <alifsakib@gmail.com>
2022-11-08 13:40:58 +0600

    added conditional rendering to review page

--- src/components/Pages/ServiceDetails/ServiceDetails.js
+++ src/components/Pages/ServiceDetails/ServiceDetails.js
@@ -1,12 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import toast from "react-hot-toast";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
+import { AuthContext } from "../../../contexts/AuthProvider";
 import Reviews from "./Reviews";
 
 const ServiceDetails = () => {
   const service = useLoaderData();
   const { title, img, description, price, _id, service_id } = service.data;
   const [reviews, setReviews] = useState([]);
+  const { user } = useContext(AuthContext);
   useEffect(() => {
     fetch(`http://localhost:5000/reviews/${service_id}`)
       .then((res) => res.json())
@@ -124,7 +126,7 @@ const ServiceDetails = () => {
       {/* Reviews */}
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20 text-start">
         <div className="flex justify-between flex-col lg:flex-row">
-          <div>
+          <div className="w-1/2">
             {/* <div className="lg:pr-10">
               <a
                 href="/"
@@ -166,7 +168,100 @@ const ServiceDetails = () => {
           </div>
           <div>
             <div className="w-full max-w-xl ">
-              <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+              {user?.uid ? (
+                <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+                  <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                    Add Review
+                  </h3>
+                  <form onSubmit={handleSubmitReview}>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="firstName"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        First name
+                      </label>
+                      <input
+                        placeholder="John"
+                        required
+                        type="text"
+                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="firstName"
+                        name="firstName"
+                      />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="lastName"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        Last name
+                      </label>
+                      <input
+                        placeholder="Doe"
+                        required
+                        type="text"
+                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="lastName"
+                        name="lastName"
+                      />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="email"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        E-mail
+                      </label>
+                      <input
+                        placeholder="john.doe@example.org"
+                        required
+                        type="text"
+                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="email"
+                        name="email"
+                      />
+                    </div>
+                    <div className="mb-1 sm:mb-2">
+                      <label
+                        htmlFor="email"
+                        className="inline-block mb-1 font-medium"
+                      >
+                        Message
+                      </label>
+                      <textarea
+                        placeholder="Type your text here ."
+                        required
+                        type="text"
+                        className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                        id="message"
+                        name="message"
+                      />
+                    </div>
+                    <div className="mt-4 mb-2 sm:mb-4">
+                      <button
+                        type="submit"
+                        className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                      >
+                        Submit
+                      </button>
+                    </div>
+                    <p className="text-xs text-gray-600 sm:text-sm">
+                      Thank you for your valuable review.
+                    </p>
+                  </form>
+                </div>
+              ) : (
+                <div>
+                  <Link
+                    to="/login"
+                    className="inline-flex items-center justify-center h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md hover:text-deep-purple-900 bg-teal-accent-400 hover:bg-deep-purple-accent-100 focus:shadow-outline focus:outline-none"
+                  >
+                    Please login to add a review.
+                  </Link>
+                </div>
+              )}
+              {/* <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
                 <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
                   Add Review
                 </h3>
@@ -247,7 +342,7 @@ const ServiceDetails = () => {
                     Thank you for your valuable review.
                   </p>
                 </form>
-              </div>
+              </div> */}
             </div>
           </div>
         </div>
