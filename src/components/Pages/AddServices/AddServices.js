2b72be0470fb1a4eae19b32baee30bb38afd1450
AlifSakib <alifsakib@gmail.com>
2022-11-08 18:37:58 +0600

    added add services section

--- src/components/Pages/AddServices/AddServices.js
+++ src/components/Pages/AddServices/AddServices.js
@@ -1,7 +1,116 @@
 import React from "react";
+import AddServiceHeader from "./AddServiceHeader";
 
 const AddServices = () => {
-  return <div>AddServices</div>;
+  return (
+    <div>
+      <AddServiceHeader></AddServiceHeader>
+      <div>
+        <section class="text-gray-600 body-font relative text-start">
+          <div class="container px-5 py-24 mx-auto flex sm:flex-nowrap flex-wrap">
+            <div className="bg-white rounded shadow-2xl p-7 sm:p-10">
+              <h3 className="mb-4 text-xl font-semibold sm:text-center sm:mb-6 sm:text-2xl">
+                Add Review
+              </h3>
+              <form>
+                <div className="mb-1 sm:mb-2">
+                  <label
+                    htmlFor="firstName"
+                    className="inline-block mb-1 font-medium"
+                  >
+                    Service ID
+                  </label>
+                  <input
+                    placeholder="John"
+                    required
+                    type="text"
+                    className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="serviceid"
+                    name="serviceid"
+                  />
+                </div>
+                <div className="mb-1 sm:mb-2">
+                  <label
+                    htmlFor="lastName"
+                    className="inline-block mb-1 font-medium"
+                  >
+                    Title
+                  </label>
+                  <input
+                    placeholder="Doe"
+                    required
+                    type="text"
+                    className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="title"
+                    name="title"
+                  />
+                </div>
+                <div className="mb-1 sm:mb-2">
+                  <label
+                    htmlFor="email"
+                    className="inline-block mb-1 font-medium"
+                  >
+                    Image
+                  </label>
+                  <input
+                    placeholder="john.doe@example.org"
+                    required
+                    type="text"
+                    className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="image"
+                    name="image"
+                  />
+                </div>
+                <div className="mb-1 sm:mb-2">
+                  <label
+                    htmlFor="email"
+                    className="inline-block mb-1 font-medium"
+                  >
+                    Price
+                  </label>
+                  <input
+                    placeholder="john.doe@example.org"
+                    required
+                    type="text"
+                    className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="price"
+                    name="price"
+                  />
+                </div>
+                <div className="mb-1 sm:mb-2">
+                  <label
+                    htmlFor="email"
+                    className="inline-block mb-1 font-medium"
+                  >
+                    Description
+                  </label>
+                  <textarea
+                    placeholder="Type your text here ."
+                    required
+                    type="text"
+                    className="flex-grow w-full h-12 px-4 mb-2 transition duration-200 bg-white border border-gray-300 rounded shadow-sm appearance-none focus:border-deep-purple-accent-400 focus:outline-none focus:shadow-outline"
+                    id="description"
+                    name="description"
+                  />
+                </div>
+                <div className="mt-4 mb-2 sm:mb-4">
+                  <button
+                    type="submit"
+                    className="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  >
+                    Submit
+                  </button>
+                </div>
+                <p className="text-xs text-gray-600 sm:text-sm">
+                  Keep your services alive
+                </p>
+              </form>
+            </div>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default AddServices;
--- src/router/PrivateRoute.js
+++ src/router/PrivateRoute.js
@@ -1,11 +1,16 @@
 import React, { useContext } from "react";
 import { Navigate } from "react-router-dom";
+import { MoonLoader } from "react-spinners";
 import { AuthContext } from "../contexts/AuthProvider";
 
 const PrivateRoute = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex justify-center items-center my-16">
+        <MoonLoader color="#36d7b7" />
+      </div>
+    );
   }
 
   if (user) {
